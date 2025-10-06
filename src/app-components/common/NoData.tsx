7cc45c10a02e392dfda75c57b58c48e5501ac5b7
uzair-shafi <uzairshaffi654@gmail.com>
2025-10-06 22:44:27 +0500

    minor changes
    Signed-off-by: uzair-shafi <uzairshaffi654@gmail.com>

--- src/app-components/common/NoData.tsx
+++ src/app-components/common/NoData.tsx
@@ -2,7 +2,7 @@ import { FolderOpen } from "lucide-react";
 
 export default function NoDataAvailable() {
   return (
-    <div className="flex flex-col items-center justify-center py-12 text-slate-500">
+    <div className="flex flex-col items-center justify-center py-20 text-slate-500">
       <FolderOpen className="w-9 h-9 mb-1 text-slate-400" strokeWidth={1.3} />
       <p className="text-[14px] font-semibold">No Data Available</p>
     </div>
